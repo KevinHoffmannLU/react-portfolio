8472309aa936c72721e7c40b460b4d9df222e2e1
Kevin <contact@hoffmannkevin.com>
2024-03-03 19:29:10 +0100

    adding basic navbar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import NavBar from './components/nav-bar/nav-bar.component';
+
+const App = () => {
+    return (
+    <div className=''>
+        <NavBar />
+    </div>
+    );
+    }
+
+export default App;
---
+++ src/components/nav-bar/nav-bar.component.jsx
@@ -0,0 +1,14 @@
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div className="bg-[#27272a] border-b-2 border-white flex justify-end w-full h-[2.5em] items-center gap-4 p-2 font-bold pr-4">
+                <h1>PROJECTS</h1>
+                <h1>CV</h1>
+                <h1>CONTACT</h1>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
